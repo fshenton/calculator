71beeed62dee89ff885a6b7fbdac98e6164d0d1b
fshenton <fshenton@gmail.com>
2019-07-17 15:26:24 +0100

    Sorted out the flex alignment of the header and calculator and touched up some of the grid formatting.

--- calculator.js
+++ calculator.js
@@ -14,29 +14,26 @@ function init(){
 
 	//add a click event listener to every number-button
 	for(button of numberButtons){
-		button.addEventListener("click", addToScreen);
+		button.addEventListener("click", addNumberToScreen);
 	}
-
-	function addToScreen(event){
-		//prevents the form buttons from triggering a refresh
-		event.preventDefault();
-
-		//grabs the button that triggered the click event
-		const button 						= event.target;
-		//grab the data-value from the buttons dataset
-		const { value: buttonValue } 		= button.dataset;
-		//grab the value from the screen
-		const { innerText: screenValue } 	= calculatorScreen;
-		//add the new value to the existing one
-		const newScreenValue 				= screenValue + buttonValue;
-
-		//change the current screen value to the new value
-		screenValue.innerText = newScreenValue;
-	}// addToScreen
-
-
-
 }// init
 
+
+function addNumberToScreen(event){
+	//prevents the form buttons from triggering a refresh
+	event.preventDefault();
+
+	//grabs the button that triggered the click event
+	const button 						= event.target;
+	//grab the data-value from the buttons dataset
+	const { value: buttonValue } 		= button.dataset;
+	//grab the value from the screen
+	const { innerText: screenValue } 	= calculatorScreen;
+	//add the new value to the existing one
+	const newScreenValue 				= screenValue + buttonValue;
+
+	//change the current screen value to the new value
+	screenValue.innerText = newScreenValue;
+}// addToScreen
 
 
