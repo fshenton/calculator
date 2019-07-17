9bbac7ad79512a6dda4472e5297302935d5e02e8
fshenton <fshenton@gmail.com>
2019-07-17 14:47:01 +0100

    Refactored the buttons code in HTML, CSS and JS to make it more readable and to reduce duplication.

--- calculator.js
+++ calculator.js
@@ -1,123 +1,42 @@
 
-const powerButton = document.getElementById("powerbutton");
-const calculatorScreen = document.getElementById("calculatorscreen");
 
-const zeroButton = document.getElementById("zerobutton");
-const oneButton = document.getElementById("onebutton");
-const twoButton = document.getElementById("twobutton");
-const threeButton = document.getElementById("threebutton");
-const fourButton = document.getElementById("fourbutton");
-const fiveButton = document.getElementById("fivebutton");
-const sixButton = document.getElementById("sixbutton");
-const sevenButton = document.getElementById("sevenbutton");
-const eightButton = document.getElementById("eightbutton");
-const nineButton = document.getElementById("ninebutton");
+window.addEventListener("DOMContentLoaded", init);
 
 
-// other, better form?
-// document.getElementById("myAnchor").addEventListener("click", function(event){
-//   blah blah
-// });
 
+function init(){
 
-// FUNCTION BUTTONS
-powerButton.onclick = function(event) { 
-	event.preventDefault();
-	calculatorScreen.style.backgroundColor = "#BBBBBB";
-	//calculatorScreen.setAttribute("background-color", "#BBBBBB");
-	// ^ unclear why that doesn't work
-	calculatorScreen.innerText = "0";
-}
+	const powerButton = document.getElementsByClassName("power button");
 
-// NUMBER BUTTONS!
+	const calculatorScreen = document.getElementById("screen");
 
-// sooooo much duplication ;_;
+	const numberButtons = document.getElementsByClassName("number button");
 
-zeroButton.onclick = function(event){
-	event.preventDefault()
-	if(calculatorScreen.innerText !== "0"){
-		calculatorScreen.innerText += "0";
+	//add a click event listener to every number-button
+	for(button of numberButtons){
+		button.addEventListener("click", addToScreen);
 	}
-}
 
-oneButton.onclick = function(event){
-	event.preventDefault()
-	if(calculatorScreen.innerText !== "0"){
-		calculatorScreen.innerText += "1"
-	} else { 
-		calculatorScreen.innerText = "1" 
-	}
-}
+	function addToScreen(event){
+		//prevents the form buttons from triggering a refresh
+		event.preventDefault();
 
-twoButton.onclick = function(event){
-	event.preventDefault()
-	if(calculatorScreen.innerText !== "0"){
-		calculatorScreen.innerText += "2";
-	} else { 
-		calculatorScreen.innerText = "2" 
-	}
-}
+		//grabs the button that triggered the click event
+		const button 						= event.target;
+		//grab the data-value from the buttons dataset
+		const { value: buttonValue } 		= button.dataset;
+		//grab the value from the screen
+		const { innerText: screenValue } 	= calculatorScreen;
+		//add the new value to the existing one
+		const newScreenValue 				= screenValue + buttonValue;
 
-threeButton.onclick = function(event){
-	event.preventDefault()
-	if(calculatorScreen.innerText !== "0"){
-		calculatorScreen.innerText += "3";
-	} else { 
-		calculatorScreen.innerText = "3" 
-	}
-}
+		//change the current screen value to the new value
+		screenValue.innerText = newScreenValue;
+	}// addToScreen
 
-fourButton.onclick = function(event){
-	event.preventDefault()
-	if(calculatorScreen.innerText !== "0"){
-		calculatorScreen.innerText += "4";
-	} else { 
-		calculatorScreen.innerText = "4" 
-	}
-}
 
-fiveButton.onclick = function(event){
-	event.preventDefault()
-	if(calculatorScreen.innerText !== "0"){
-		calculatorScreen.innerText += "5";
-	} else { 
-		calculatorScreen.innerText = "5" 
-	}
-}
 
-sixButton.onclick = function(event){
-	event.preventDefault()
-	if(calculatorScreen.innerText !== "0"){
-		calculatorScreen.innerText += "6";
-	} else { 
-		calculatorScreen.innerText = "6" 
-	}
-}
+}// init
 
-sevenButton.onclick = function(event){
-	event.preventDefault()
-	if(calculatorScreen.innerText !== "0"){
-		calculatorScreen.innerText += "7";
-	} else { 
-		calculatorScreen.innerText = "7" 
-	}
-}
 
-eightButton.onclick = function(event){
-	event.preventDefault()
-	if(calculatorScreen.innerText !== "0"){
-		calculatorScreen.innerText += "8";
-	} else { 
-		calculatorScreen.innerText = "8" 
-	}
-}
-
-nineButton.onclick = function(event){
-	event.preventDefault()
-	if(calculatorScreen.innerText !== "0"){
-		calculatorScreen.innerText += "9";
-	} else { 
-		calculatorScreen.innerText = "9" 
-	}
-}
 
