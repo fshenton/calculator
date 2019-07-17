3a801206a0f344c0f8fb3e928e765911d3fe8909
fshenton <fshenton@gmail.com>
2019-07-17 16:37:20 +0100

    New power button handler adds a CSS class to the screen which changes the background. I also added a new font (digital-7 (mono)).

--- calculator.js
+++ calculator.js
@@ -8,15 +8,38 @@ function init(){
 
 	const powerButton = document.getElementsByClassName("power button");
 
-	const calculatorScreen = document.getElementById("screen");
-
 	const numberButtons = document.getElementsByClassName("number button");
+
+	powerButton[0].addEventListener("click", handlePowerPressed);
 
 	//add a click event listener to every number-button
 	for(button of numberButtons){
 		button.addEventListener("click", addNumberToScreen);
 	}
 }// init
+
+
+function handlePowerPressed(event){
+	//prevents the form buttons from triggering a refresh
+	event.preventDefault();
+
+	//grab the button that fired trigger the click event
+	const button = event.target;
+
+	//declare the class name to be checked for and added if not present
+	const powerOnClass = "powered";
+
+	const screen = document.getElementById("screen");
+
+	//try to find if the screen is powered
+	if(!screen.classList.contains(powerOnClass)){
+		//add the class if it's not there
+		screen.classList.add(powerOnClass);
+	}
+
+	//set the screen to show zero
+	screen.innerText = "0";
+}//handlePowerPressed
 
 
 function addNumberToScreen(event){
@@ -36,4 +59,7 @@ function addNumberToScreen(event){
 	screenValue.innerText = newScreenValue;
 }// addToScreen
 
+function add(num1, num2){
+
+}
 
