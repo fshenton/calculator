1fe8711659128f7d904daa288e8d1f26492bfd24
fshenton <fshenton@gmail.com>
2019-07-18 16:17:05 +0100

    Decimal button implemented, basics operator's HTML refactored to use data-action. HandleOperatorPressed function being implemented.

--- calculator.js
+++ calculator.js
@@ -5,18 +5,26 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init(){
 
-	const powerButton = document.getElementsByClassName("power button");
+	const powerButtons = document.getElementsByClassName("power button");
 	const numberButtons = document.getElementsByClassName("number button");
+	const decimalButtons = document.getElementsByClassName("decimal button");
+	const operatorButtons = document.getElementsByClassName("operator button");
 	const screen = document.getElementById("screen");
 	const powered = "powered";
 
-	powerButton[0].addEventListener("click", handlePowerPressed);
-
+	powerButtons[0].addEventListener("click", handlePowerPressed);
+	
 	//add a click event listener to every number-button
 	for(button of numberButtons){
 		button.addEventListener("click", addNumberToScreen);
 	}
 
+	decimalButtons[0].addEventListener("click", addDecimalToScreen);
+	
+	//add a click event listener to every number-button
+	for(button of operatorButtons){
+		button.addEventListener("click", handleOperatorPressed);
+	}
 
 	function handlePowerPressed(event){
 		//prevents the form buttons from triggering a refresh
@@ -54,7 +62,7 @@ function init(){
 			let newScreenValue = 0;
 
 			//don't append new number to new value if current value is 0 (prevents "0134"...)
-			if(screen.innerText === "0"){
+			if(screenValue === "0"){
 				newScreenValue = buttonValue;
 			}
 			else{
@@ -67,54 +75,37 @@ function init(){
 
 	}// addNumberToScreen
 
-	function add(num1, num2){
 
-	}//add
+	function addDecimalToScreen(event){
+		//prevents the form buttons from triggering a refresh
+		event.preventDefault();
 
+		//don't do anything else if the screen isn't turned on
+		if(screen.classList.contains(powered)){
+			
+			//grabs the button that triggered the click event
+			const button 						= event.target;
 
-	function subtract(num1, num2){
+			//grab the value from the screen
+			const { innerText: screenValue } 	= screen;
+			
+			if(!screenValue.includes(".")){
+				screen.innerText = screenValue + '.';
+			}
+		}
+	}
 
-	}//subtract
+	function handleOperatorPressed(event){
+		//prevents the form buttons from triggering a refresh
+		event.preventDefault();
 
+		if(screen.classList.contains(powered)){
+			
+			const button = event.target;
 
-	function multiply(num1, num2){
-
-	}//multiply
-
-
-	function divide(num1, num2){
-
-	}//divide
-
-
-	function sqrt(num){
-
-	}//sqrt
-
-
-	function index(num){
-
-	}//index
-
-
-	function equals(num){
-
-	}//equals
-
-	function mrc(num){
-
-	}//mrc
-	
-
-	function memminus(num){
-
-	}//memminus
-
-
-	function memmplus(num){
-
-	}//memmplus
-
+			const { action } = button.dataset;
+		}
+	}
 
 }// init
 
